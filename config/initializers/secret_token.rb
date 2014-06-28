abf19e5b1bffecedac15b600cbf10ff036cf5437
ikegamihideomi <heidiswagger@gmail.com>
2014-06-28 12:30:21 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamuraiApp::Application.config.secret_key_base = 'cd4622f6591195498c2dd3eaed4d048e0635b18392920811e31cc6b11269d0f305864fe46dc337efae26c969f4d017c107916a36c14f7e9f8d1fd90c6980be70'
